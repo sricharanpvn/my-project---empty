a657db1ce1987314f36f290c1cce7938db05b824
sricharanpvn <sricharan.pvn@gmail.com>
2022-12-20 14:43:18 +0530

    20 Dec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from './api.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-project';
+  data: any;
+  urldata: any;
+  // searchText: any;
+  searchText= '';
+  page:number = 1;
+  count:number = 0;
+  tableSize:number = 3;
+  lat:number=35.5;
+  lon:number=-78.5;
+
+  constructor( private service:ApiService, private fb:FormBuilder) {}//
+  ngOnInit(){
+    this.data=this.service.getdata().subscribe((urldata)=>{
+      this.data=urldata;
+    });
+
+    this.urldata=this.service.geturldata().subscribe((ud)=>{
+      this.urldata=ud;
+    });
+
+    this.service.geturldata().
+  }
+
+  // latlondetails:any = this.fb.group({
+  //   lat:[''],
+  //   lon:['']
+  // });
+
+  onTableDataChange(event:any){
+    this.page = event;
+    this.data;
+  }
+  submit(){
+
+  }
 }
